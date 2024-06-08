4bf03fa2b1f623059b5ec48f2d65b85806bf43e1
RamyAdell <ramyadelshawky@hotmail.com>
2024-06-08 07:21:46 +0300

    doMyJop

---
+++ src/DoMyJop/DoMyJop.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DoMyJop;
+
+
+
+/**
+ *
+ * @author Rami
+ */
+public class DoMyJop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       NewJFrame s = new NewJFrame();
+        s.setVisible(true);
+        s.setAlwaysOnTop(true);
+        s.setLocationRelativeTo(null);
+     
+        
+        // TODO code application logic here
+    }
+    
+}
